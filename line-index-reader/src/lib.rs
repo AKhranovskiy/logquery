21d3b62e9e977caa657041da34c1280e975773c5
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2024-05-06 22:22:38 +0700

    LineIndexReader
    LineIndexReader indexes new lines in a text file and offers reading lines by index/number.

---
+++ line-index-reader/src/lib.rs
@@ -0,0 +1,228 @@
+use std::{
+    io::{BufRead, Seek, SeekFrom},
+    ops::{Bound, RangeBounds},
+    path::{Path, PathBuf},
+};
+
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncSeekExt, BufReader},
+    spawn,
+    sync::{mpsc, oneshot},
+    task::spawn_blocking,
+};
+
+const READ_BUF_CAPACITY: usize = 8_192;
+
+type LineRequest = (
+    // Offset in the file, in bytes
+    u64,
+    // Limit reading in bytes. If `None`, read until the end of the file.
+    Option<usize>,
+    // Sender for the response
+    oneshot::Sender<Result<Box<[String]>, Error>>,
+);
+
+pub struct LineIndexReader {
+    path: PathBuf,
+    offsets: Vec<u64>,
+    tx: mpsc::Sender<LineRequest>,
+}
+
+/// Common interface
+impl LineIndexReader {
+    pub async fn index<P>(path: P) -> Result<Self, Error>
+    where
+        P: AsRef<Path> + Clone + Send,
+    {
+        let file = File::open(path.clone()).await?;
+        let (file, offsets) = spawn_blocking(move || index_lines(file)).await.unwrap()?;
+
+        let (tx, mut rx) = mpsc::channel::<LineRequest>(10);
+
+        spawn(async move {
+            let mut reader = BufReader::new(file);
+            while let Some((offset, limit, resp)) = rx.recv().await {
+                let result = read_lines(&mut reader, offset, limit).await;
+                let _ = resp.send(result);
+            }
+        });
+
+        Ok(Self {
+            path: path.as_ref().to_owned(),
+            offsets,
+            tx,
+        })
+    }
+
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.offsets.len()
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    #[must_use]
+    pub async fn get_line(&self, line: usize) -> Option<String> {
+        self.get_lines(line..=line)
+            .await
+            .and_then(|v| v.first().cloned())
+    }
+
+    #[must_use]
+    pub async fn get_lines<R>(&self, range: R) -> Option<Box<[String]>>
+    where
+        R: RangeBounds<usize> + Send,
+    {
+        let offset = {
+            let start = match range.start_bound() {
+                Bound::Included(x) => *x,
+                Bound::Excluded(x) => *x + 1,
+                Bound::Unbounded => 0,
+            };
+            self.offsets.get(start).copied()?
+        };
+
+        let end = match range.end_bound() {
+            Bound::Included(x) => Some(*x + 1),
+            Bound::Excluded(x) => Some(*x),
+            Bound::Unbounded => None,
+        };
+
+        let limit = end
+            .and_then(|end| self.offsets.get(end))
+            .or_else(|| self.offsets.last())
+            .and_then(|v| v.checked_sub(offset))
+            .and_then(|v| usize::try_from(v).ok());
+
+        let (tx, rx) = oneshot::channel();
+        self.tx.send((offset, limit, tx)).await.ok()?;
+
+        rx.await.ok()?.ok()
+    }
+
+    pub async fn update(&mut self) -> Result<usize, Error> {
+        if let Ok(index) = self.consistency().await?.into_inconsistent() {
+            return Err(Error::InconsistentIndex(index));
+        }
+
+        let old_len = self.offsets.len();
+        let offset = self.offsets.last().copied().unwrap_or_default();
+
+        let mut file = File::open(&self.path).await?;
+        let pos = file.seek(SeekFrom::Start(offset)).await?;
+        assert_eq!(pos, offset);
+
+        let (_, offsets) = spawn_blocking(move || index_lines(file)).await.unwrap()?;
+        self.offsets.extend(&offsets[1..]);
+
+        Ok(self.offsets.len() - old_len)
+    }
+
+    /// Verifies that the index is consistent with the file.
+    /// Return `true` if the index is consistent, `false` otherwise.
+    pub async fn consistency(&self) -> Result<IndexConsistency, Error> {
+        let mut file = File::open(&self.path).await?;
+        let file_len = file.metadata().await?.len();
+
+        for (index, &offset) in self.offsets.iter().enumerate().skip(1) {
+            assert!(offset > 0);
+            let offset = offset - 1;
+
+            if offset > file_len {
+                dbg!(1);
+                return Ok(IndexConsistency::Inconsistent(index));
+            }
+
+            if offset != file.seek(SeekFrom::Start(offset)).await? {
+                dbg!(2);
+                return Ok(IndexConsistency::Inconsistent(index));
+            }
+
+            let byte = file.read_u8().await?;
+            if b'\n' != byte {
+                dbg!(byte as char);
+                return Ok(IndexConsistency::Inconsistent(index));
+            }
+        }
+
+        Ok(IndexConsistency::Consistent)
+    }
+}
+
+#[derive(Debug, Clone, Copy, enum_as_inner::EnumAsInner, PartialEq, Eq)]
+pub enum IndexConsistency {
+    Consistent,
+    Inconsistent(usize),
+}
+
+async fn read_lines(
+    reader: &mut BufReader<File>,
+    offset: u64,
+    limit: Option<usize>,
+) -> Result<Box<[String]>, Error> {
+    let pos = reader.seek(SeekFrom::Start(offset)).await?;
+    assert_eq!(pos, offset);
+
+    let buf = if let Some(limit) = limit {
+        let mut buf = Vec::with_capacity(limit);
+        reader.read_buf(&mut buf).await?;
+        buf
+    } else {
+        // Dangerous!!! Reading without the limit.
+        let mut buf = Vec::with_capacity(READ_BUF_CAPACITY);
+        reader.read_to_end(&mut buf).await?;
+        buf
+    };
+
+    // Reading from the mem buf, no need for async.
+    std::io::BufReader::new(std::io::Cursor::new(buf))
+        .lines()
+        .collect::<Result<Vec<_>, _>>()
+        .map(Vec::into_boxed_slice)
+        .map_err(Into::into)
+}
+
+fn index_lines(file: tokio::fs::File) -> Result<(tokio::fs::File, std::vec::Vec<u64>), Error> {
+    let mut file = file.try_into_std().unwrap();
+
+    let mut offsets = vec![];
+
+    let mut offset = file.stream_position()?;
+    let mut buf = String::with_capacity(READ_BUF_CAPACITY);
+    let mut reader = std::io::BufReader::new(&file);
+
+    // TODO handle very long lines: read in chunks until the hard limit.
+    while let Ok(read_bytes) = reader.read_line(&mut buf) {
+        if read_bytes == 0 {
+            break; // EOF
+        }
+
+        offsets.push(offset);
+
+        if buf.chars().nth(read_bytes - 1) != Some('\n') {
+            // No EOL, we've reached the end of the file.
+            break;
+        }
+        buf.clear();
+
+        offset += read_bytes as u64;
+
+        assert_eq!(reader.stream_position()?, offset);
+    }
+
+    file.rewind().unwrap();
+
+    Ok((File::from_std(file), offsets))
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("Inconsistent index at line {0}")]
+    InconsistentIndex(usize),
+}
---
+++ line-index-reader/tests/tests.rs
@@ -0,0 +1,205 @@
+use std::{io::Write, ops::RangeBounds};
+
+use line_index_reader::LineIndexReader;
+use tempfile::NamedTempFile;
+
+#[rstest::rstest]
+#[case::empty(empty(), 0)]
+#[case::one_line_with_eof(one_line_eol(), 1)]
+#[case::one_line_no_eof(one_line(), 1)]
+#[case::small_no_eof(small_file(), SMALL_FILE_LINES)]
+#[case::small_with_eof(small_file_eol(), SMALL_FILE_LINES)]
+#[case::large(large_with_eof(), LARGE_FILE_LINES)]
+#[tokio::test]
+pub async fn index(#[case] file: NamedTempFile, #[case] expected_len: usize) {
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+
+    assert_eq!(index.len(), expected_len);
+}
+
+#[rstest::rstest]
+#[case::first(0, "Line 000000".into())]
+#[case::middle(SMALL_FILE_LINES / 2, "Line 004782".into())]
+#[case::last(SMALL_FILE_LINES-1, "Line 009564".into())]
+#[case::eof(SMALL_FILE_LINES, None)]
+#[case::last_plus_one(SMALL_FILE_LINES + 1, None)]
+#[case::beyond_eof(SMALL_FILE_LINES + 10, None)]
+#[tokio::test]
+pub async fn read_single_line(#[case] line: usize, #[case] expected: Option<&'static str>) {
+    let file = small_file();
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+
+    assert_eq!(expected, index.get_line(line).await.as_deref());
+}
+
+#[rstest::rstest]
+#[case::from_start(..10, 11 * 10)]
+#[case::beginning(0..10, 11 * 10)]
+#[case::middle(SMALL_FILE_LINES / 3 .. SMALL_FILE_LINES / 2, 11 * 1_594)]
+#[case::end(SMALL_FILE_LINES-10..SMALL_FILE_LINES, 11 * 9)]
+#[case::eof(SMALL_FILE_LINES-10.., 11 * 9)]
+#[case::beyond_eof(SMALL_FILE_LINES.., 0)]
+#[tokio::test]
+pub async fn read_many_lines<R>(#[case] lines: R, #[case] expected_size: usize)
+where
+    R: RangeBounds<usize> + Send,
+{
+    let file = small_file_eol();
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+    let lines = index.get_lines(lines).await;
+
+    assert_eq!(
+        expected_size,
+        lines
+            .map(|v| v.iter().map(String::len).sum())
+            .unwrap_or_default()
+    );
+}
+
+#[rstest::rstest]
+#[case::no_lines(0)]
+#[case::one_line(1)]
+#[case::many_lines(9)]
+#[tokio::test]
+pub async fn update(#[case] new_lines: usize) {
+    let mut file = one_line();
+
+    let mut index = LineIndexReader::index(&file).await.expect("LineIndex");
+    assert_eq!(1, index.len());
+
+    {
+        for i in 1..=new_lines {
+            write!(file, "\nLine {i:06}").unwrap();
+        }
+        file.flush().unwrap();
+    }
+
+    assert_eq!(new_lines, index.update().await.expect("Updated index"));
+    assert_eq!(1 + new_lines, index.len());
+}
+
+#[rstest::rstest]
+#[case::empty(empty())]
+#[case::one(one_line())]
+#[case::one_eol(one_line_eol())]
+#[case::small(small_file())]
+#[case::small_eol(small_file_eol())]
+#[tokio::test]
+pub async fn consistency(#[case] file: NamedTempFile) {
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+
+    assert!(index
+        .consistency()
+        .await
+        .expect("Index consistency")
+        .is_consistent());
+}
+
+#[tokio::test]
+pub async fn consistency_on_truncated() {
+    let mut file = temp_file(10);
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+
+    file.as_file_mut().set_len(11 * 5).expect("Truncated file");
+
+    assert_eq!(
+        5,
+        index
+            .consistency()
+            .await
+            .expect("Index consistency")
+            .into_inconsistent()
+            .expect("Inconsitent index")
+    );
+}
+
+#[tokio::test]
+pub async fn consistency_on_appended() {
+    let mut file = temp_file(10);
+    let index = LineIndexReader::index(&file).await.expect("LineIndex");
+
+    for i in 10..15 {
+        writeln!(file, "Line {i:06}").unwrap();
+    }
+
+    file.flush().unwrap();
+
+    assert!(index
+        .consistency()
+        .await
+        .expect("Index consistency")
+        .is_consistent());
+}
+
+// 11 bytes per line, so under 100K lines
+const SMALL_FILE_LINES: usize = 9_565;
+// 11 bytes per line, so over 100K lines
+const LARGE_FILE_LINES: usize = 123_456;
+
+fn temp_file(lines: usize) -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    for i in 0..lines {
+        writeln!(f, "Line {i:06}").unwrap();
+    }
+
+    f.flush().unwrap();
+    f
+}
+
+#[rstest::fixture]
+#[once]
+fn empty() -> tempfile::NamedTempFile {
+    tempfile::NamedTempFile::new().unwrap()
+}
+
+#[rstest::fixture]
+#[once]
+fn one_line_eol() -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    writeln!(f, "Line 000000").unwrap();
+    f.flush().unwrap();
+    f
+}
+
+#[rstest::fixture]
+#[once]
+fn one_line() -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    write!(f, "Line 000000").unwrap();
+    f.flush().unwrap();
+    f
+}
+
+#[rstest::fixture]
+#[once]
+fn small_file_eol() -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    for i in 0..SMALL_FILE_LINES {
+        writeln!(f, "Line {i:06}").unwrap();
+    }
+    f.flush().unwrap();
+    f
+}
+
+#[rstest::fixture]
+#[once]
+fn small_file() -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    for i in 0..SMALL_FILE_LINES - 1 {
+        writeln!(f, "Line {i:06}").unwrap();
+    }
+    write!(f, "Line {:06}", SMALL_FILE_LINES - 1).unwrap();
+    f.flush().unwrap();
+    f
+}
+
+#[rstest::fixture]
+#[once]
+fn large_with_eof() -> tempfile::NamedTempFile {
+    let mut f = tempfile::NamedTempFile::new().unwrap();
+    for i in 0..LARGE_FILE_LINES {
+        writeln!(f, "Line {i:06}").unwrap();
+    }
+    f.flush().unwrap();
+    f
+}
